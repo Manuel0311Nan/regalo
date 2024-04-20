fb66545d2b0ab924d5ddaedab0cc80897d9fe586
Manuel0311Nan <manuzz3112@gmail.com>
2024-04-20 14:20:02 +0200

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,49 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+"use client"
+import { useState } from 'react';
 
 export default function Home() {
+  const [currentSection, setCurrentSection] = useState(0);
+  const sections = [
+  { title: "Sección 1", description: "Si creías que abrir este regalo sería fácil, ¡sorpresa! ", imageUrl: "/images/seccion1.jpg" },
+  { title: "Sección 2", description: "Esperamos que tengas tiempo suficiente...", imageUrl: "/images/seccion2.jpg" },
+  { title: "Sección 3", description: "¡No te rindas ahora! Estás a poca distancia del premio.", imageUrl: "/images/seccion3.jpg" },
+  { title: "Sección 4", description: "Si estás pensando en pedir ayuda, recuerda: el que persevera alcanza... ¡el regalo!", imageUrl: "/images/seccion4.jpg" },
+  { title: "Sección 5", description: "Pensamos que este osito enfadado protegería mejor el regalo que nosotros", imageUrl: "/images/seccion5.jpg" },
+  { title: "Sección 6", description: "Te daremos 3 opciones...", imageUrl: "/images/seccion6.jpg" },
+  { title: "Sección 7", description: "¿Será un libro?", imageUrl: "/images/seccion7.jpg" },
+  { title: "Sección 8", description: "¿Puede que un viaje?", imageUrl: "/images/seccion8.jpg" },
+  { title: "Sección 9", description: "Alomejor se come", imageUrl: "/images/seccion9.jpg" },
+  { title: "Sección 10", description: "Descúbrelo tú misma, nosotros no nos acordamos de lo que era ya", imageUrl: "/images/seccion10.jpg" },
+];
+
+  const goToNextSection = () => {
+    if (currentSection < sections.length - 1) {
+      setCurrentSection(currentSection + 1);
+      document.getElementById(`section-${currentSection + 1}`).scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div class="flex flex-col">
+    <div class="rounded-md bg-blue-500 p-2 w-96 mx-auto my-5">
+      <h1 class="text-center text-xl ">Esperamos que te guste by</h1>
+      <h1 class="sticky text-center text-xl">Dani & Manu</h1>
+    </div>
+
+      {sections.map((section, index) => (
+        <div key={index} id={`section-${index}`} style={{ height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+          <img src={section.imageUrl} alt={`Imagen de ${section.title}`} style={{ maxWidth: '100%', maxHeight: '300px' }} class="w-96 h-auto" />
+          <p class="w-96 text-center ">{section.description}</p>
+          {index < 9 ? (
+          <button onClick={goToNextSection} class="text-xl rounded-md bg-blue-500 p-2 mt-2">
+            Sigue buscando
+          </button>
+        ) : (
+          <a href="/documents/regaloSilvia.pdf" download="Regalo.pdf" class="text-xl rounded-md bg-blue-500 p-2 w-52">Descargar Virus</a>
+        )}
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      ))}
+    </div>
   );
 }
